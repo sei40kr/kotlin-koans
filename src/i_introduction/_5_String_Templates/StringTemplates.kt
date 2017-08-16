2a859d61e00b6a230b22adfaa2751e2aaf01f2ae
Seong Yong-ju <sei40kr@gmail.com>
2017-08-16 21:29:48 +0900

    Completed i_introduction
    Completed i_introduction. Passed all unit tests.

--- src/i_introduction/_5_String_Templates/StringTemplates.kt
+++ src/i_introduction/_5_String_Templates/StringTemplates.kt
@@ -3,16 +3,13 @@ package i_introduction._5_String_Templates
 import util.TODO
 import util.doc5
 
-fun example1(a: Any, b: Any) =
-        "This is some text in which variables ($a, $b) appear."
+fun example1(a : Any, b : Any) = "This is some text in which variables ($a, $b) appear."
 
-fun example2(a: Any, b: Any) =
-        "You can write it in a Java way as well. Like this: " + a + ", " + b + "!"
+fun example2(a : Any, b : Any) = "You can write it in a Java way as well. Like this: " + a + ", " + b + "!"
 
-fun example3(c: Boolean, x: Int, y: Int) = "Any expression can be used: ${if (c) x else y}"
+fun example3(c : Boolean, x : Int, y : Int) = "Any expression can be used: ${if (c) x else y}"
 
-fun example4() =
-        """
+fun example4() = """
 You can use raw strings to write multiline text.
 There is no escaping here, so raw strings are useful for writing regex patterns,
 you don't need to escape a backslash by a backslash.
@@ -25,14 +22,12 @@ fun example() = "13.06.1992".matches(getPattern().toRegex()) //true
 
 val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
 
-fun todoTask5(): Nothing = TODO(
-    """
+fun todoTask5() : Nothing = TODO("""
         Task 5.
         Copy the body of 'getPattern()' to the 'task5()' function
         and rewrite it in such a way that it matches '13 JUN 1992'.
         Use the 'month' variable.
-    """,
-    documentation = doc5(),
-    references = { getPattern(); month })
+    """, documentation = doc5(), references = { getPattern(); month })
 
-fun task5(): String = todoTask5()
+fun task5() : String = """\d{2}\s+$month\s+\d{4}"""
+
--- src/i_introduction/_8_Smart_Casts/SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/SmartCasts.kt
@@ -4,21 +4,17 @@ import util.TODO
 import util.doc8
 
 interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
+class Num(val value : Int) : Expr
+class Sum(val left : Expr, val right : Expr) : Expr
 
-fun eval(e: Expr): Int =
-        when (e) {
-            is Num -> todoTask8(e)
-            is Sum -> todoTask8(e)
-            else -> throw IllegalArgumentException("Unknown expression")
-        }
+fun eval(e : Expr) : Int = when (e) {
+  is Num -> e.value
+  is Sum -> eval(e.left) + eval(e.right)
+  else -> throw IllegalArgumentException("Unknown expression")
+}
 
-fun todoTask8(expr: Expr): Nothing = TODO(
-    """
+fun todoTask8(expr : Expr) : Nothing = TODO("""
         Task 8.
         Rewrite 'JavaCode8.eval()' in Kotlin using smart casts and 'when' expression.
-    """,
-    documentation = doc8(),
-    references = { JavaCode8().eval(expr) })
+    """, documentation = doc8(), references = { JavaCode8().eval(expr) })
 
