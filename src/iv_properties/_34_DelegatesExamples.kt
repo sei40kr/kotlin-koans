ac54d842ae72b448846fab0e6f4e545e4bb9f7ab
Seong Yong-ju <sei40kr@gmail.com>
2017-08-17 01:24:59 +0900

    Completed iv_properties
    Completed iv_properties. Passed all unit tests.

--- src/iv_properties/_34_DelegatesExamples.kt
+++ src/iv_properties/_34_DelegatesExamples.kt
@@ -2,15 +2,26 @@ package iv_properties
 
 import util.TODO
 import util.doc34
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
-class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+class LazyPropertyUsingDelegates(initializer : () -> Int) {
+  val lazyValue : Int by Delegate(initializer)
 }
 
-fun todoTask34(): Lazy<Int> = TODO(
-    """
+internal class Delegate<T>(private val initializer : () -> T) : ReadOnlyProperty<LazyPropertyUsingDelegates, T> {
+  private var value : T? = null
+
+  override fun getValue(thisRef : LazyPropertyUsingDelegates, property : KProperty<*>) : T {
+    if (value == null) {
+      value = initializer()
+    }
+
+    return value!!
+  }
+}
+
+fun todoTask34() : Lazy<Int> = TODO("""
         Task 34.
         Read about delegated properties and make the property lazy by using delegates.
-    """,
-    documentation = doc34()
-)
+    """, documentation = doc34())
